53fc233481a61152e3b79db2507e34ae4fc17d44
Gieza <giedrzav@gmail.com>
2018-07-13 11:07:55 +0300

    Creation of the project

---
+++ src/main/java/espOtaServer/EspOtaServerApplication.java
@@ -0,0 +1,12 @@
+package espOtaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspOtaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspOtaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/espOtaServer/EspOtaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package espOtaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EspOtaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
